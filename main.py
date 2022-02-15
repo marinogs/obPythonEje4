06809020821bc734b49b0eea9397e34c0aeea702
Marino Guadia Suárez <marinoguardiasuarez@gmail.com>
2022-02-15 09:42:11 +0100

    Primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#OpenBootcamp Curso Python Ejercicio 4
+
+# Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edad = int(input("Cual es su edad?"))
+
+if edad < 18 :
+    print("Eres menor de edad.")
+else:
+    print("Eres mayor de edad")
+
+
